ad3b7ec7b267004ac529a4d1ffdb20110be474b1
tRumiBe <152004872+tRumiBe@users.noreply.github.com>
2024-09-27 16:16:41 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#app',
+    data: {
+        intro: '<h1>Phishing Awareness Training</h1>',
+        message: "<h2>Oh no! You should not have clicked on the link!</h2><br><p>Why Is Phishing Awareness Important?</p><p>Phishing attacks is a type of cyberattack that trick users into giving away sensitive information like passwords or credit card numbers. As our internship projects involve email communication, it's important to recognize the signs of phishing attempts.</p><p>Click here to read our new post about phishing. I promise, no more tricks!</p><br>",
+        button: "<a href=\"https://www.dmeinterns.org\" class=\"btn btn-primary text-white px-5 py-3\">Learn More</a>"
+    }
+});
